dc132d019c5a99bbccbb31ce702fe7c9c48257a6
“tumnbv” <tumanbaevtair@gmail.com>
2023-05-29 11:36:30 +0300

    first commit

---
+++ src/com/java/class1/practice1.java
@@ -0,0 +1,9 @@
+package com.java.class1;
+
+public class practice1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
---
+++ src/com/java/main.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
